bb9b66810f509e643f82d48d972862fbd69646a5
msok9559-cyber <msok9559@gmail.com>
2025-11-28 16:12:00 +0900

    Create Example01.java

---
+++ 14-java/src/day01/Example01.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Example01 {
+    static void main() {
+        int x = 10;
+        int y = 3;
+        System.out.printf("x = %d, y = %d\n", x, y);
+        System.out.printf("x + y = %d\n", x + y);
+        System.out.printf("x - y = %d\n", x - y);
+        System.out.printf("x * y = %d\n", x * y);
+        System.out.printf("x / y = %.2f\n", (double)x / y);
+        System.out.printf("x %% y = %d\n", x % y);
+    }
+}
