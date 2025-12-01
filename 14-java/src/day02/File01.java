41531d6c080cc9e27c76c092810788e84503398c
msok9559-cyber <msok9559@gmail.com>
2025-12-01 17:49:05 +0900

    Create File01.java

---
+++ 14-java/src/day02/File01.java
@@ -0,0 +1,13 @@
+package day02;
+
+import java.util.Scanner;
+
+public class File01 {
+    static void main() {
+        Scanner input  = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = input.nextInt();
+
+
+    }
+}
