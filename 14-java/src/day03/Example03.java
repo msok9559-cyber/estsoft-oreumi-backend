cf23f0560e9878e63870fbc061049f01dfc673d4
msok9559-cyber <msok9559@gmail.com>
2025-12-03 17:39:20 +0900

    Create Example03.java

---
+++ 14-java/src/day03/Example03.java
@@ -0,0 +1,21 @@
+package day03;
+
+import java.util.Scanner;
+
+public class Example03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = input.nextInt();
+        input.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i -1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
