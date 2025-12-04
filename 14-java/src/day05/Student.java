c8a61017e5a3d1b0783c10410446369f68886863
msok9559-cyber <msok9559@gmail.com>
2025-12-04 16:05:51 +0900

    Create Student.java

---
+++ 14-java/src/day05/Student.java
@@ -0,0 +1,41 @@
+package day05;
+
+public class Student {
+    private String name;
+    private int korean;
+    private int math;
+    private int english;
+    private double average;
+    private char grade;
+
+    public String getName() {return name;}
+    public int getKorean() {return korean;}
+    public int getMath() {return math;}
+    public int getEnglish() {return english;}
+    public double getAverage() {return average;}
+    public char getGrade() {return grade;}
+
+    public void setName(String name) {this.name = name;}
+    public void setKorean(int korean) {this.korean = korean;}
+    public void setMath(int math) {this.math = math;}
+    public void setEnglish(int english) {this.english = english;}
+    public void setAverage(double average) {
+        this.average = (korean + math + english) / 3.0;
+    }
+    public void setGrade(char grade) {
+        if (average >= 90) grade = 'A';
+        else if (average >= 80) grade = 'B';
+        else if (average >= 70) grade = 'C';
+        else if (average >= 60) grade = 'D';
+        else grade = 'F';
+        this.grade = grade;
+
+    }
+    public void print() {
+        System.out.println("Name: " + name);
+        System.out.printf("Average: %.1f\n " , average);
+        System.out.println("Grade: " + grade);
+    }
+
+
+}
