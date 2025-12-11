0143d408465e1b17a840323bb82eb1b95150139c
msok9559-cyber <msok9559@gmail.com>
2025-12-11 17:52:56 +0900

    Update Solution.java

--- 14-java/src/day10/Solution.java
+++ 14-java/src/day10/Solution.java
@@ -1,21 +1,28 @@
 package day10;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
-    public int[] solution(int[] arr, int divisor ) {
-        int[] answer = Arrays.stream(arr)
-                .filter(n -> n % divisor == 0)
-                .sorted()
-                .toArray();
-        return answer.length == 0 ? new int[]{-1} : answer;
+    public int[] solution(int[] arr){
+        List<Integer> list = new ArrayList<>();
+
+        list.add(arr[0]);
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] != arr[i-1]){
+                list.add(arr[i]);
+            }
+        }
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        int[] arr = {5, 9, 7, 10};
-        int divisor = 5;
-        int[] result = s.solution(arr, divisor);
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+        int[] result = s.solution(arr);
         System.out.println(Arrays.toString(result));
     }
 }
