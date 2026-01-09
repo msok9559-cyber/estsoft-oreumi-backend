023a5b01f95417c88dfd47b1172ece6369e58f3e
msok9559-cyber <msok9559@gmail.com>
2026-01-09 17:19:05 +0900

    수정

--- 32-java-backend/step01/src/main/java/com/example/step01/ex05/gugudan.java
+++ 32-java-backend/step01/src/main/java/com/example/step01/ex05/gugudan.java
@@ -15,9 +15,10 @@ public class gugudan extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
         String danParam = request.getParameter("dan");
+        response.setContentType("text/html;charset=UTF-8");
 
         String output = """
                 <!DOCTYPE html>
